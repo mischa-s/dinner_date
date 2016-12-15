77d128f482f48706d7b5c71ef48b4f643b9755da
mischa-saunders <mischa.saunders@gmail.com>
2016-12-16 10:41:56 +1300

    added handlebars to dependencies

--- seeds/friends.js
+++ seeds/friends.js
@@ -1,13 +1,17 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('friends').del()
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('table_name').insert({id: 1, colName: 'rowValue1'}),
-        knex('table_name').insert({id: 2, colName: 'rowValue2'}),
-        knex('table_name').insert({id: 3, colName: 'rowValue3'})
+        knex('friends').insert({id: 1, friend_name: 'Sue'}),
+        knex('friends').insert({id: 2, friend_name: 'Leonard'}),
+        knex('friends').insert({id: 3, friend_name: 'Sally'}),
+        knex('friends').insert({id: 4, friend_name: 'Paul'}),
+        knex('friends').insert({id: 5, friend_name: 'Meghan'}),
+        knex('friends').insert({id: 6, friend_name: 'Mischa'}),
+        knex('friends').insert({id: 7, friend_name: 'Janie'})
       ]);
     });
 };
